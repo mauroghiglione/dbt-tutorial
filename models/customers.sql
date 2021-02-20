211b1efe06e0da599a70cdf2cf9c0c87981647c7
Mauro <mauro.ghiglione@sky.uk>
2021-02-20 13:03:53 +0000

    models/customers.sql

--- models/customers.sql
+++ models/customers.sql
@@ -1,11 +1,11 @@
-with customers as (
+with cust as (
     select
         id as customer_id,
         first_name,
         last_name
     from dbt_mauro.customers
 ),
-orders as (
+ord as (
     select
         id as order_id,
         user_id as customer_id,
@@ -20,7 +20,7 @@ customer_orders as (
         min(order_date) as first_order_date,
         max(order_date) as most_recent_order_date,
         count(order_id) as number_of_orders
-    from orders
+    from ord
     group by 1
 ),
 
@@ -28,14 +28,14 @@ customer_orders as (
 final as (
 
     select
-        customers.customer_id,
-        customers.first_name,
-        customers.last_name,
+        cust.customer_id,
+        cust.first_name,
+        cust.last_name,
         customer_orders.first_order_date,
         customer_orders.most_recent_order_date,
         coalesce(customer_orders.number_of_orders, 0) as number_of_orders
 
-    from customers
+    from cust
 
     left join customer_orders using (customer_id)
 
